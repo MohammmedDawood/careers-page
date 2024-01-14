8cfd8fbfddf3940da25e319bdb1734ca124a2a3f
Mohammed Dawood <mahmedtony73@gmail.com>
2024-01-15 02:26:38 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "careers-page",
   server: {
     // env.PORT is injected by loadEnv()
     // port: parseInt(env.PORT) || 3000,
